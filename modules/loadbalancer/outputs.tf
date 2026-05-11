4947de303e97f1597807b9ea22241a410f7c52d1
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 22:48:31 +0530

    Update outputs.tf

--- modules/loadbalancer/outputs.tf
+++ modules/loadbalancer/outputs.tf
@@ -1,11 +1,7 @@
 output "lb_ip" {
-  value = google_compute_global_forwarding_rule.forwarding_rule.ip_address
+  value = google_compute_global_address.lb_ip.address
 }
 
 output "application_url" {
   value = "https://${var.fqdn}"
-}
-
-output "lb_ip" {
-  value = google_compute_global_address.lb_ip.address
 }
