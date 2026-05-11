fbee40d6f969bba894b451ecf4417e18d8b2130a
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 00:29:04 +0530

    Define variables for firewall module

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,1 +1,23 @@
-variable "network" {}
+variable "network" {
+  type = string
+}
+
+variable "name" {
+  type    = string
+  default = "allow-ssh-http"
+}
+
+variable "ports" {
+  type    = list(string)
+  default = ["22", "80"]
+}
+
+variable "source_ranges" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "target_tags" {
+  type    = list(string)
+  default = ["web-server"]
+}
