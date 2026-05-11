1695685a2aa22a31d02978489995e929f09e768e
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 15:33:49 +0530

    Create main.tf

---
+++ modules/mig/main.tf
@@ -0,0 +1,46 @@
+resource "google_compute_instance_template" "template" {
+
+  name_prefix  = var.template_name
+  machine_type = var.machine_type
+
+  tags = ["http-server"]
+
+  disk {
+    source_image = "debian-cloud/debian-12"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    subnetwork = var.subnetwork
+
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_public_key}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "mig" {
+
+  name   = var.mig_name
+  region = var.region
+
+  version {
+    instance_template = google_compute_instance_template.template.id
+  }
+
+  base_instance_name = "cloudscaleops"
+
+  target_size = var.target_size
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+}
