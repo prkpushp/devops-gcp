5d2b91dd989d0b14d6f8d1282cad3260bc0a8504
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 13:56:57 +0530

    Update main.tf

--- cloudconfigops/terraform/environments/dev/main.tf
+++ cloudconfigops/terraform/environments/dev/main.tf
@@ -1,27 +1,87 @@
+module "apis" {
+
+  source = "../../../../modules/apis"
+
+  project_id = var.project_id
+
+  apis = var.gcp_apis
+}
+
 module "network" {
+
+  depends_on = [module.apis]
+
   source = "../../../../modules/network"
+
   network_name = var.network_name
-  subnet_name  = var.subnet_name
-  subnet_cidr  = var.subnet_cidr
+
+  subnet_name = var.subnet_name
+
+  subnet_cidr = var.subnet_cidr
+
   region = var.region
 }
 
 module "firewall" {
+
   source = "../../../../modules/firewall"
+
   network = module.network.network_name
 }
 
-module "web_vm" {
+# =====================================================
+# HAProxy VM
+# =====================================================
+
+module "haproxy_vm" {
+
   source = "../../../../modules/compute"
-  instance_name = var.instance_name
-  machine_type = var.machine_type
+
+  instance_name = "haproxy-vm"
+
+  machine_type = "e2-micro"
+
   zone = var.zone
+
   subnetwork = module.network.subnet_name
+
   ssh_public_key = var.ssh_public_key
 }
 
-module "apis" {
-  source = "../../../../modules/apis"
-  project_id = var.project_id
-  apis = var.gcp_apis
+# =====================================================
+# APP VM 1
+# =====================================================
+
+module "app_vm_1" {
+
+  source = "../../../../modules/compute"
+
+  instance_name = "app-vm-1"
+
+  machine_type = "e2-micro"
+
+  zone = var.zone
+
+  subnetwork = module.network.subnet_name
+
+  ssh_public_key = var.ssh_public_key
+}
+
+# =====================================================
+# APP VM 2
+# =====================================================
+
+module "app_vm_2" {
+
+  source = "../../../../modules/compute"
+
+  instance_name = "app-vm-2"
+
+  machine_type = "e2-micro"
+
+  zone = var.zone
+
+  subnetwork = module.network.subnet_name
+
+  ssh_public_key = var.ssh_public_key
 }
