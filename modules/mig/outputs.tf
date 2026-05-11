e1b09daab49fa077cacdab5df2c2287bd2a8d36a
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 15:49:08 +0530

    Update outputs.tf

--- modules/mig/outputs.tf
+++ modules/mig/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_group" {
   value = google_compute_region_instance_group_manager.mig.instance_group
 }
+
+output "instance_group_manager" {
+  value = google_compute_region_instance_group_manager.mig.id
+}
