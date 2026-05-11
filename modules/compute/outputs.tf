6276673932821fe3742c664313bf0b33e331f59b
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 14:19:32 +0530

    Update outputs.tf

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
+
   value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "private_ip" {
+
+  value = google_compute_instance.vm.network_interface[0].network_ip
 }
