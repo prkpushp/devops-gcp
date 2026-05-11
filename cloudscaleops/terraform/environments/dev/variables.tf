abc41e76f31fbff56253d7c6b915c060725e2954
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 15:38:57 +0530

    Create variables.tf

---
+++ cloudscaleops/terraform/environments/dev/variables.tf
@@ -0,0 +1,47 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "template_name" {
+  type = string
+}
+
+variable "mig_name" {
+  type = string
+}
+
+variable "target_size" {
+  type = number
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "gcp_apis" {
+  type = list(string)
+}
