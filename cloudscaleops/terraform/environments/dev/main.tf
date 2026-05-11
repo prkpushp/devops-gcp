d5753ee0aaff3e96cf4b754474b9d0234a1d99ca
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 21:36:53 +0530

    Update main.tf

--- cloudscaleops/terraform/environments/dev/main.tf
+++ cloudscaleops/terraform/environments/dev/main.tf
@@ -32,7 +32,7 @@ module "loadbalancer" {
   source         = "../../../../modules/loadbalancer"
   name           = "cloudscaleops"
   instance_group = module.mig.instance_group
-  domain_name = local.fqdn
+  fqdn           = local.fqdn
 }
 
 module "autoscaler" {
