9c80ca1b3d8057788ee8e085a6c8cb6e703361d0
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 14:20:08 +0530

    Update outputs.tf

--- cloudconfigops/terraform/environments/dev/outputs.tf
+++ cloudconfigops/terraform/environments/dev/outputs.tf
@@ -1,19 +1,24 @@
 output "haproxy_public_ip" {
-  value = module.haproxy_vm.instance_public_ip
+
+  value = module.haproxy_vm.public_ip
 }
 
 output "app_vm_1_private_ip" {
-  value = module.app_vm_1.instance_private_ip
+
+  value = module.app_vm_1.private_ip
 }
 
 output "app_vm_2_private_ip" {
-  value = module.app_vm_2.instance_private_ip
+
+  value = module.app_vm_2.private_ip
 }
 
 output "app_vm_1_public_ip" {
-  value = module.app_vm_1.instance_public_ip
+
+  value = module.app_vm_1.public_ip
 }
 
 output "app_vm_2_public_ip" {
-  value = module.app_vm_2.instance_public_ip
+
+  value = module.app_vm_2.public_ip
 }
