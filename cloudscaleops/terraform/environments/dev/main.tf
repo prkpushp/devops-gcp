8cdd680487ef82495b1c1b2d5797260622bf8c55
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-11 15:36:25 +0530

    Create main.tf

---
+++ cloudscaleops/terraform/environments/dev/main.tf
@@ -0,0 +1,42 @@
+module "apis" {
+  source     = "../../../../modules/apis"
+  project_id = var.project_id
+  apis       = var.gcp_apis
+}
+
+module "network" {
+  source       = "../../../../modules/network"
+  network_name = var.network_name
+  subnet_name  = var.subnet_name
+  subnet_cidr  = var.subnet_cidr
+  region       = var.region
+}
+
+module "firewall" {
+  source  = "../../../../modules/firewall"
+  network = module.network.network_name
+}
+
+module "mig" {
+  source         = "../../../../modules/mig"
+  template_name  = var.template_name
+  machine_type   = var.machine_type
+  subnetwork     = module.network.subnet_name
+  ssh_public_key = var.ssh_public_key
+  mig_name       = var.mig_name
+  region         = var.region
+  target_size    = var.target_size
+}
+
+module "loadbalancer" {
+  source         = "../../../../modules/loadbalancer"
+  name           = "cloudscaleops"
+  instance_group = module.mig.instance_group
+}
+
+module "autoscaler" {
+  source = "../../../../modules/autoscaler"
+  name   = "cloudscaleops-autoscaler"
+  region = var.region
+  target = module.mig.instance_group
+}
