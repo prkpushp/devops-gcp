ecbc5d8fcca4d7aee333df36292a9b7499895159
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 07:46:45 +0530

    Update variables.tf

--- monitoring/terraform/variables.tf
+++ monitoring/terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "instance_name" {}
 variable "machine_type" { default = "e2-micro" }
 variable "zone" {}
-variable "subnetwork" {}
 variable "ssh_public_key" {}
 variable "instance_tag" {}
 
@@ -12,7 +11,10 @@ variable "firewall_target_tags" {
   description = "Target tags for firewall rules"
   type        = list(string)
 }
-variable "network" {
-  description = "GCP VPC network name"
-  type        = string
+variable "network_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
 }
