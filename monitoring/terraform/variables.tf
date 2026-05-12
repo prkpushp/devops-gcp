9c44999bb7a646115b98e52fdca14a3dd020c21b
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 07:18:12 +0530

    Update variables.tf

--- monitoring/terraform/variables.tf
+++ monitoring/terraform/variables.tf
@@ -3,12 +3,12 @@ variable "machine_type" { default = "e2-micro" }
 variable "zone" {}
 variable "subnetwork" {}
 variable "ssh_public_key" {}
-variable "monitoring_machine_type" {}
-variable "monitoring_instance_tag" {}
+variable "machine_type" {}
+variable "instance_tag" {}
 
-variable "monitoring_firewall_name" {}
-variable "monitoring_firewall_ports" {}
-variable "monitoring_source_ranges" {}
+variable "firewall_name" {}
+variable "firewall_ports" {}
+variable "firewall_source_ranges" {}
 variable "network" {
   description = "GCP VPC network name"
   type        = string
