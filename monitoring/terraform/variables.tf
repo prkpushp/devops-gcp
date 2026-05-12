6ef50435906de58985ccc4eb163b21073bd9b67b
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 07:20:11 +0530

    Update variables.tf

--- monitoring/terraform/variables.tf
+++ monitoring/terraform/variables.tf
@@ -3,7 +3,6 @@ variable "machine_type" { default = "e2-micro" }
 variable "zone" {}
 variable "subnetwork" {}
 variable "ssh_public_key" {}
-variable "machine_type" {}
 variable "instance_tag" {}
 
 variable "firewall_name" {}
