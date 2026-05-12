6a374019ed81e8d40d205a843f0e934fb3019e85
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 07:31:01 +0530

    Update variables.tf

--- monitoring/terraform/variables.tf
+++ monitoring/terraform/variables.tf
@@ -8,10 +8,6 @@ variable "instance_tag" {}
 variable "firewall_name" {}
 variable "firewall_ports" {}
 variable "firewall_source_ranges" {}
-variable "network" {
-  description = "GCP VPC network name"
-  type        = string
-}
 variable "firewall_target_tags" {
   description = "Target tags for firewall rules"
   type        = list(string)
