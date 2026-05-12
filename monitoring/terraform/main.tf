844a68447f18d44d4ca10a0e81bfe01db57f8462
Pushp Ranjan Karn <prkpushp@gmail.com>
2026-05-12 07:23:16 +0530

    Update main.tf

--- monitoring/terraform/main.tf
+++ monitoring/terraform/main.tf
@@ -2,11 +2,11 @@ module "monitoring_vm" {
   source = "../../modules/compute"
 
   instance_name  = "monitoring-vm"
-  machine_type   = var.monitoring_machine_type
+  machine_type   = var.machine_type
   zone           = var.zone
   subnetwork     = var.subnetwork
 
-  instance_tag   = var.monitoring_instance_tag
+  instance_tag   = var.instance_tag
 
   ssh_public_key = var.ssh_public_key
 }
@@ -15,11 +15,11 @@ module "monitoring_vm" {
 module "monitoring_firewall" {
   source = "../../modules/firewall"
 
-  name          = var.monitoring_firewall_name
+  name          = var.firewall_name 
   network       = var.network
 
-  ports         = var.monitoring_firewall_ports
-  source_ranges = var.monitoring_source_ranges
+  ports         = var.firewall_ports
+  source_ranges = var.firewall_source_ranges 
 
-  target_tags   = [var.monitoring_instance_tag]
+  target_tags   = [var.firewall_target_tags]
 }
